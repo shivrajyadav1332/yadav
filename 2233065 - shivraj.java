0b51ec7d9471df3eb212711dc9f85a58bd80a6c2
shivrajyadav1332 <Shivrajy4716@gmail.com>
2025-03-27 17:29:16 +0530

    Update 2233065 - shivraj.java

--- 2233065 - shivraj.java
+++ 2233065 - shivraj.java
@@ -28,38 +28,8 @@ class Institute {
     private String institute_name;
     private Student student;
 
-    // Getters and Setters
-    public String getInstitute_name() {
-        return institute_name;
-    }
-
-    public void setInstitute_name(String institute_name) {
-        this.institute_name = institute_name;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
-    }
 }
+    
 
-public class JsonParser {
-    public static void main(String[] args) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            // Read JSON file
-            Institute institute = objectMapper.readValue(new File("input.json"), Institute.class);
+    
 
-            // Display parsed data
-            System.out.println("Institute Name: " + institute.getInstitute_name());
-            System.out.println("Student Name: " + institute.getStudent().getFirst_name());
-            System.out.println("Roll Number: " + institute.getStudent().getRoll_number());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
